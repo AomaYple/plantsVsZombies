3403459ce9f64513a8951e46efade884bbddb78d
AomaYple <2890599214@qq.com>
2024-04-04 17:28:21 +0800

    为exe程序添加图标

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 auto main(int argc, char *argv[]) -> int {
     const QGuiApplication application{argc, argv};
 
-    QGuiApplication::setWindowIcon(QIcon{"resources/scenes/favicon.ico"});
     const QQmlApplicationEngine engine{"src/main.qml"};
 
     return QGuiApplication::exec();
