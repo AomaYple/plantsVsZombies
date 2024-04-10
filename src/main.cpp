9e69fd6d3856ca1119f5d511298f42db211cb057
AomaYple <2890599214@qq.com>
2024-04-10 17:06:13 +0800

    移除无用头文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <QGuiApplication>
-#include <QIcon>
 #include <QQmlApplicationEngine>
-#include <QQmlContext>
 
 auto main(int argc, char *argv[]) -> int {
     const QGuiApplication application{argc, argv};
