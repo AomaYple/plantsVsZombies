9a80a215d78c69dd666bf6d3699a8e6baecf750f
AomaYple <2890599214@qq.com>
2024-05-17 21:56:54 +0800

    设定应用图标

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <QGuiApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 
 auto main(int argc, char *argv[]) -> int {
     const QGuiApplication application{argc, argv};
+    QGuiApplication::setWindowIcon(QIcon{"resources/favicon.ico"});
 
     const QQmlApplicationEngine engine{"src/main.qml"};
 
